982d7a8fd4604f4820ed1e506635a28851de61d2
Punyapushya <167755921+Punyapushya@users.noreply.github.com>
2025-04-23 15:10:05 +0530

    Update genai_video_summarisation_final.py

--- genai_video_summarisation_final.py
+++ genai_video_summarisation_final.py
@@ -84,8 +84,8 @@ def turn_into_story(summary):
     return model.invoke(prompt)
 
 # Streamlit UI
-st.title("📺 PragyanAI - YouTube/Uploaded Video Summarizer Using Groq LLM")
-st.image("PragyanAI_Transperent.png")
+st.title("📺 Punya - YouTube/Uploaded Video Summarizer Using Groq LLM")
+#st.image("PragyanAI_Transperent.png")
 
 youtube_url = st.text_input("Paste a YouTube video URL:", placeholder="https://www.youtube.com/watch?v=example")
 
